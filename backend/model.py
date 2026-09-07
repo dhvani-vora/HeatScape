32f04cfc7169a90d5c702686dbceeba7e29841b5
jananikm4 <jananikm4@gmail.com>
2026-09-07 22:39:42 +0530

    Add Hotspot model using Pydantic

--- backend/model.py
+++ backend/model.py
@@ -1,1 +1,11 @@
+from pydantic import BaseModel
 
+class Hotspot(BaseModel):
+    id: int
+    lat: float
+    lng: float
+    lst: float
+    ndvi: float
+    built_up: float
+    population: float
+    risk: float
